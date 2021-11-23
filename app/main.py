4bac2b6beb2297f325bb403a6ad3150d2af2b2f7
Timothy Bryant <timothybryant3@gmail.com>
2021-11-22 23:39:34 -0500

    Add routes and package folder

--- app/main.py
+++ app/main.py
@@ -20,6 +20,11 @@ def find_post(id):
         if post["id"] == id:
             return post
 
+def find_index_post(id):
+    for index, post in enumerate(my_posts):
+        if post["id"] == id:
+            return index
+
 @app.get("/")
 def root(): # "async def..." is optional if you want to use it
     return {"message": "Hello World"}
@@ -34,12 +39,12 @@ def create_posts(post: Post):
     return {"data": post_dict}
 
 @app.get("/posts/{id}")
-def get_posts(id: int): # id is a path parameter: int is the type of the parameter using pydantic
+def get_post(id: int): # id is a path parameter: int is the type of the parameter using pydantic
     post = find_post(id)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail= f"post with {id} not found")
-    #     response.status_code = status.HTTP_404_NOT_FOUND # you can also set the status code like this but ned to include response: Response path parameter
+    #     response.status_code = status.HTTP_404_NOT_FOUND # you can also set the status code like this but need to include response: Response path parameter
     #     return {"message": f"post with {id} not found"}
     return {"data": post}
 
@@ -47,6 +52,22 @@ def get_posts(id: int): # id is a path parameter: int is the type of the paramet
 def get_posts():
     return {"data": my_posts}
 
-# @app.put("/posts/{id}")
+@app.put("/posts/{id}")
+def update_posts(id: int, post: Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail= f"post with {id} not found")
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
 
-# @app.delete("/posts/{id}")
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_posts(id: int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail= f"post with {id} not found")
+    my_posts.remove(post)
+    return {"message": f"post with {id} deleted"}
