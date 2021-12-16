e40212d0f174c30f00509d0aed694be6de254b19
Tim Bryant <timothybryant3@gmail.com>
2021-12-16 01:36:21 -0500

    Add last tests for users, posts, votes

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,9 +7,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.main import app
 
+from app.oauth2 import create_access_token
 from app.config import settings
 from app.database import get_db
 from app.database import Base
+from app import models
 # from alembic import command
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
@@ -20,7 +22,7 @@ TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engin
 @pytest.fixture
 def session():
     # print("my session scope='fixture' ran") # scopes can be set such as session(scope="module")
-    Base.metadata.drop_all(bind=engine) # running it here allows you to see data in db after each test
+    Base.metadata.drop_all(bind=engine) # running it here allows you to see data in db after each test since db is not deleted until next tests is run
     Base.metadata.create_all(bind=engine)
     # command.upgrade("head") # if we wanted to use alembic
     db = TestingSessionLocal()
@@ -50,3 +52,70 @@ def test_user(client):
     new_user = res.json()
     new_user['password'] = user_data['password']
     return new_user
+
+#############################################################################################
+
+@pytest.fixture
+def test_user2(client):
+    user_data = {"email": "timothy123@gmail.com", "password": "password123"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id": test_user['id']})
+
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+
+    return client
+
+
+@pytest.fixture
+def test_posts(test_user, session, test_user2):
+    posts_data = [{
+        "title": "first title",
+        "content": "first content",
+        "user_id": test_user['id']
+    }, {
+        "title": "2nd title",
+        "content": "2nd content",
+        "user_id": test_user['id']
+    },
+        {
+        "title": "3rd title",
+        "content": "3rd content",
+        "user_id": test_user['id']
+    }, {
+        "title": "3rd title",
+        "content": "3rd content",
+        "user_id": test_user2['id']
+    }]
+
+    def create_post_model(post):
+        return models.Post(**post)
+
+    # map(function, list_data)
+    post_map = map(create_post_model, posts_data)
+    posts = list(post_map)
+
+    session.add_all(posts)
+
+    # could have used this method to add posts to db
+    # session.add_all([models.Post(title="first title", content="first content", user_id=test_user['id']),
+    #                 models.Post(title="2nd title", content="2nd content", user_id=test_user['id']), models.Post(title="3rd title", content="3rd content", user_id=test_user['id'])])
+    # session.commit()
+    # session.query(models.Post).all()
+
+    session.commit()
+    posts = session.query(models.Post).all()
+    return posts
--- tests/test_users.py
+++ tests/test_users.py
@@ -30,3 +30,22 @@ def test_login_user(test_user,client):
     assert id == test_user['id']
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+# def test_incorrect_login(test_user, client):
+#     res = client.post("/login", data={"username": test_user[ 'email'], "password": "wrongPassword"})
+#     assert res.status_code == 403
+#     assert res.json().get('detail') == 'Invalid credentials'
+
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('sanjeev@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('sanjeev@gmail.com', None, 422)])
+
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+    assert res.status_code == status_code
+
+
